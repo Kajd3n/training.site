aebbb8b22211cbab4df0c77bbd6d2a6feac9849f
Kajd3n <alexander.kajden@gmail.com>
2021-06-10 19:23:29 +0300

    switch strings

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     $title = 'Тренировочный сайт';
 
     require('components/header.php');
-    require('components/header.php');
+    require('components/content.php');
     require('components/content.php');
     require('components/sidebar.php');
     require('components/footer.php');
