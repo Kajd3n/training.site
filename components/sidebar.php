8039b203f819a70a81c4a73b3efa2d61536f9fb2
Kajd3n <alexander.kajden@gmail.com>
2021-06-12 18:13:01 +0300

    some commit

--- components/sidebar.php
+++ components/sidebar.php
@@ -2,7 +2,7 @@
     <div class="in-sidebar">
         <h2>Sidebar</h2>
         <ul class="menu">
-            <li><a href="#">Item #1</a></li>
+            <li><a>Item #1</a></li>
             <li><a href="#">Item #2</a></li>
             <li><a href="#">Item #3</a></li>
         </ul>
