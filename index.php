5414225424516741c9d9f14aa1c9d5178dd9a9ab
Kajd3n <alexander.kajden@gmail.com>
2021-06-16 21:56:06 +0300

    Delete PHP-files

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$title = 'Тренировочный сайт';
-
-	require('components/header.php');
-	require('components/content.php');
-	require('components/sidebar.php');
-	require('components/footer.php');
-?>
