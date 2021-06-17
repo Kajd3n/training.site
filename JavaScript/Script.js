cb08f24c4a059d7c36cd06d7f48b4edacf2df3a7
Kajd3n <alexander.kajden@gmail.com>
2021-06-17 20:38:32 +0300

    added JS on page

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,7 +1,20 @@
 let button = document.querySelector('.button');
-let title = document.querySelector('title');
+let inContent = document.querySelector('.in-content');
+let counter = 4;
 
 button.onclick = function() {
+	let article = document.createElement('article');
+	let h3 = document.createElement('h3');
+	let p = document.createElement('p');
 	
 
+	h3.textContent = 'Block of content #' + counter++;
+	p.textContent = 'There is some text here...';
+
+	article.classList.add('box');
+
+	article.append(h3);
+	article.append(p);
+	
+	inContent.append(article);
 }
